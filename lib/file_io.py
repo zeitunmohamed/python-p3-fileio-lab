a2de3aecc99f2ecbeee48cd8d5e6d0b9cacf81dd
zeitunmohamed <zeitunmohamed@gmail.com>
2025-06-23 21:22:26 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)  # Remove the extra newline
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
